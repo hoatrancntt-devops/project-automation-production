69bad2f380603b891a3923f50935ea1fc063d324
hoatrancntt-devops <hoatrancntt@gmail.com>
2026-03-30 23:21:30 +0700

    V28

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,7 +5,7 @@ output "ec2_public_ip" {
 
 output "proxmox_vm_ip" {
   description = "IP address of Proxmox VM"
-  value       = proxmox_virtual_environment_vm.db.ipv4_addresses[1][0]
+  value       = proxmox_virtual_environment_vm.rocky-cloud-init.ipv4_addresses[1][0]
 }
 
 output "alb_dns" {
